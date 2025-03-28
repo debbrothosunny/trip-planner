39be9e6475aec32a23a18103a9c8776d88099a44
debbrothosunny <debnathsunny7852@gmail.com>
2025-03-28 19:54:21 +0600

    28-03-2025

--- resources/views/backend/layouts/app.php
+++ resources/views/backend/layouts/app.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?php echo isset($header_title) ? $header_title : 'Dashboard'; ?> - University</title>
+    <title><?php echo isset($header_title) ? $header_title : 'Dashboard'; ?> -</title>
 
     <!-- Google Font: Poppins -->
     <link rel="stylesheet"
